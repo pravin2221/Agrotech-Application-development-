dcc142ba0355d6dfa21073284b4a3552ccc4fc35
pravin2221 <pravinramanaa.9119psgps2020@gmail.com>
2026-01-31 19:44:20 +0530

    Changed

--- app.py
+++ app.py
@@ -1,31 +1,22 @@
-#this is for trying out
 from argparse import Action
 from flask import Flask, request, render_template, redirect, url_for, session
 from pymongo import MongoClient
 import hashlib
 from bson.objectid import ObjectId
-import os
 
 app = Flask(__name__)
-app.secret_key = b'$\x94\xd3x&\xaf\x06\x8e>\x88d\x82\xec\xd7a\xe7jz\x88\xbf\xa2\xc93\x81'
+app.secret_key = b'$\x94\xd3x&\xaf\x06\x8e>\x88d\x82\xec\xd7a\xe7jz\x88\xbf\xa2\xc93\x81'  
+# Replace with your generated secret key
 
-# MongoDB connection string (override via env var `MONGO_URI` if needed)
-MONGO_URI = os.environ.get('MONGO_URI', 'mongodb+srv://app:agrotech@agrotech.z2op6uj.mongodb.net/?retryWrites=true&w=majority&appName=agrotech')
+# MongoDB connection string
+client = MongoClient('mongodb+srv://pravinramanaa9119psgps2020_db_user:Agrotech22@cluster0.yx4pi3r.mongodb.net/?appName=Cluster0')
 
-# Try connecting to Atlas; if it fails, fall back to an in-memory mongomock instance
-try:
-    client = MongoClient(MONGO_URI, serverSelectionTimeoutMS=5000)
-    # Force server selection / DNS resolution now so failures occur deterministically
-    client.server_info()
-    db = client['mydatabase']
-except Exception:
-    from mongomock import MongoClient as MockMongoClient
-    client = MockMongoClient()
-    db = client['mydatabase']
+# Select the database
+db = client['mydatabase']
 
 # Select the collections
 users_collection = db['users']
-crops_collection = db['crops']
+crops_collection = db['crops'] 
 
 @app.route('/')
 def index():
@@ -60,7 +51,7 @@ def login_form():
             if user['role'] == 'farmer':
                 return redirect(url_for('selection_form'))
             else:
-                return redirect(url_for('customer_page'))
+                return redirect(url_for('customer'))
         else:
             return redirect(url_for('signup_form'))
 
@@ -164,7 +155,6 @@ def buy_product():
         return f"Successfully bought {quantity} of {product['name']}!"
     else:
         return "Insufficient quantity available."
-
 
 @app.route('/cart')
 def cart():
